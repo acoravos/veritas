75473a59f39dafb3c6c873c10efb34e8b35bd1e5
acoravos <acoravos@gmail.com>
2015-08-04 08:47:27 -0700

    Update router and ajax button for SPA

--- app/assets/javascripts/App.js.jsx
+++ app/assets/javascripts/App.js.jsx
@@ -3,7 +3,26 @@
 
 App = {};
 
-App.setPath = function(path){
+App.request = function(method, path, data){
+  return new Promise(function(resolve, reject){
+    var request = $.ajax({
+      url:      path,
+      method:   method,
+      data:     data,
+      dataType: "json"
+    });
+
+    request.done(function(serverData){
+      resolve(serverData)
+    });
+
+    request.fail(function(serverData){
+      reject(serverData)
+    });
+  });
+};
+
+App.goto = function(path){
   history.pushState({}, document.title, path);
   $(window).trigger('pushstate');
 };
@@ -14,6 +33,15 @@ App.router = function(path){
   if (path === '/tweet-box') return <TweetBox />
   return <div>Page Not Found</div>
 }
+
+App.router = function(path){
+  if (path === '/')                          return <HomePage />;
+  if (path === '/tweet-box') return <TweetBox />;
+if (path.match(/^\/questions\/(\d+)$/))        return <QuestionShowPage  question_id={RegExp.$1} />;
+  return(
+    <h1>Page Not Found</h1>
+  )
+};
 
 $(function(){
   React.render(<MainComponent />, document.body);
